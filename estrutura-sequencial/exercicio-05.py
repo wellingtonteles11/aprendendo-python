3e67df4030cef2092c31a1ddcedef1c20c192460
Wellington Teles <wellingtonteles753@gmail.com>
2026-05-17 20:28:22 -0300

    resolvendo exercicio 05 de estrutura sequencial

---
+++ estrutura-sequencial/exercicio-05.py
@@ -0,0 +1,5 @@
+# Exercício 05
+# Faça um programa que converta metros para centímetros:
+medida_em_metros = int(input('Digite a quantidade em metros a ser convertida: '))
+medida_em_centimetros = medida_em_metros * 100
+print(f'{medida_em_metros}m convertida para cm é igual a {medida_em_centimetros}cm')
