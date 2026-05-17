f140bd4eda76233a270784e87d9e899505dc35a6
Wellington Teles <wellingtonteles753@gmail.com>
2026-05-17 19:55:13 -0300

    resolvendo exercicio 03 de estrutura sequencial

---
+++ estrutura-sequencial/exercicio-03.py
@@ -0,0 +1,7 @@
+# Exercício 03
+# Faça um programa que peça dois números e imprima a soma:
+primeiro_numero = int(input('Digite o primeiro número: '))
+segundo_numero = int(input('Digite o segundo número: '))
+
+soma = primeiro_numero + segundo_numero
+print(f'A soma entre {primeiro_numero} e {segundo_numero} é {soma}. ')
