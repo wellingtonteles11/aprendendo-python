9d9555b66d98848af6787700f25121b139548f79
Wellington Teles <wellingtonteles753@gmail.com>
2026-05-17 19:27:50 -0300

    resolvendo exercicio 2 de estrutura sequencial

---
+++ estrutura-sequencial/exercicio-02.py
@@ -0,0 +1,4 @@
+# Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
+
+numero_informado = input('Digite um número: ')
+print(f'O número informado foi {numero_informado}.')
