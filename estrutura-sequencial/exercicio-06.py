736df4846e825a746bdf429f16317e4a9e9d6a78
Wellington Teles <wellingtonteles753@gmail.com>
2026-05-17 22:56:30 -0300

    resolvendo exercicio 06 de estrutura sequencial

---
+++ estrutura-sequencial/exercicio-06.py
@@ -0,0 +1,7 @@
+# Exercício 06
+# Faça um programa que peça o raio de um círculo, calcule e mostre sua área:
+
+raio = float(input('Digite o valor do raio do circulo: '))
+area = 3.14 * (raio**2 )
+
+print(f'A área do circulo é: {area:.2f}')
