56e6c5e4abe36e23548918f2dcf6b8bef543b049
Wellington Teles <wellingtonteles753@gmail.com>
2026-05-17 23:06:53 -0300

    resolvendo exercicio 07 de estrutura sequencial

---
+++ estrutura-sequencial/exercicio-07.py
@@ -0,0 +1,7 @@
+# Exercício 07
+# Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado = float(input('Informe a medida do lado: '))
+area = lado ** 2
+
+print(f'A área do quadrado de lado {lado} é: {area:.2f}')
