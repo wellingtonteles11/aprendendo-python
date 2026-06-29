47ba2d0b06403f9012122ac1dba98f24ee5a8514
Wellington Teles <wellingtonteles753@gmail.com>
2026-06-28 21:39:19 -0300

    resolvendo exercicio 02 de estrutura de repeticao

--- estrutura-de-repeticao/exercicio-02.py
+++ estrutura-de-repeticao/exercicio-02.py
@@ -7,7 +7,33 @@ while True:
 
     if usuario == senha:
         print('Erro! A senha não pode ser igual ao nome de usuário.')
-        print('Tente novamente.')
+        continue
+
+    maiuscula = 0
+    minuscula = 0
+    numeros = 0
+    especiais = 0
+
+    for caractere in senha:
+        if caractere.isupper():
+            maiuscula += 1
+        elif caractere.islower():
+            minuscula += 1
+        elif caractere.isdigit():
+            numeros += 1
+        else:
+            especiais += 1
+
+    if len(senha) < 8:
+        print('A senha deve ter no mínimo 8 caracteres.')
+    elif maiuscula < 2:
+        print('A senha deve ter pelo menos 2 letras maiúculas.')
+    elif minuscula < 2:
+        print('A sneha deve ter pelo menos 2 letras minúsculas.')
+    elif numeros < 2:
+        print('A senha deve ter pelo menos 2 múmeros.')
+    elif especiais < 2:
+        print('A senha deve ter pelo menos 2 caractares especiais')
 
     else:
         print('Cadastro realizado com sucesso!')
