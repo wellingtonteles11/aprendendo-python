e5fbb455f3f556d53323af1030faca9417ee2366
Wellington Teles <wellingtonteles753@gmail.com>
2026-07-16 20:38:53 -0300

    resolvendo exercicio 12 de estrutura de repeticao

---
+++ estrutura-de-repeticao/exercicio-12.py
@@ -0,0 +1,21 @@
+# Exercício 12
+# Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
+
+# Tabuada de 5:
+# 5 X 1 = 5
+# 5 X 2 = 10
+# ...
+# 5 X 10 = 50
+
+while True:
+    numero = int(input('Digite um número entre 1 e 10: '))
+
+    if 1 <= numero <= 10:
+        break
+    print('Número inválido! Digite um número entre 1 e 10.')
+
+print(f'TABUADA DE {numero}:')
+
+for i in range(1,11):
+    print(f'{numero} X {i} = {numero * i}')
+
