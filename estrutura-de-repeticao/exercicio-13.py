0a6844472014416438d197ba25d5a67ba527ca73
Wellington Teles <wellingtonteles753@gmail.com>
2026-07-16 20:55:45 -0300

    resolvendo exercicio 13 de estrutura de repeticao

---
+++ estrutura-de-repeticao/exercicio-13.py
@@ -0,0 +1,12 @@
+# Exercício 13
+# Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
+
+base = int(input('Digite a base: '))
+expoente = int(input('Digite o expoente: '))
+
+resultado = 1
+
+for i in range(expoente):
+    resultado *= base
+
+print(f'{base} elevado a {expoente} é igual a {resultado}')
